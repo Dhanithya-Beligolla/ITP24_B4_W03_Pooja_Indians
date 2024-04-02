a9b3a96c787c9688eb454e85f6e309655f5744a0
IT22085580BGDNBeligolla <152087131+IT22085580BGDNBeligolla@users.noreply.github.com>
2024-04-02 22:49:34 +0530

    commit 7

--- backend/database/database.js
+++ backend/database/database.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.DATABASE,{}); // This is the connection string to the database
-        console.log("MongoDB connected successfully");
+        console.log("MongoDB connected fully successfully");
     } catch (error) {
         console.log(error)
     }
