3a43e89d91a04023d298758ac4e3ef29038d2b61
IT22085580BGDNBeligolla <152087131+IT22085580BGDNBeligolla@users.noreply.github.com>
2024-04-02 16:55:40 +0530

    commit 6

--- backend/routes/buffet.js
+++ backend/routes/buffet.js
@@ -4,9 +4,14 @@ const Buffet = require("../modules/buffet.js");
 const route = express.Router();
 
 
-//get all Buffet reservations user
-route.get("/", (req, res) => {
-    res.status(200).json("This is the frist buffet api");
+//get all Buffet reservations 
+route.get("/", async (req, res) => {
+    try {
+        const allBuffets = await Buffet.find({});
+        res.status(200).json({status: "SUCCESS", allBuffets});
+    } catch (error) {
+        res.status(500).json({status: "FAILED", error}); 
+    }
 });
 
 //create a Buffet reservation
@@ -19,8 +24,39 @@ route.post("/create", async (req, res) => {
     }
 });
 
+//get a one user's Buffet reservation by id
+route.get("/:id", async (req, res) => {
+    const id = req.params.id;
+    try {
+        const singleBuffet = await Buffet.findById(id);
+        res.status(200).json({status: "SUCCESS", singleBuffet});
+    } catch (error) {
+        res.status(500).json({status: "FAILED", error});
+    }
+});
+
+//update a one user's Buffet reservation by id
+route.put("/update/:id", async (req, res) => {
+    const id = req.params.id;
+    try {
+        const updatedSingleBuffet = await Buffet.findByIdAndUpdate(id, req.body);
+        res.status(200).json({status: "SUCCESS", updatedSingleBuffet});
+    } catch (error) {
+        res.status(500).json({status: "FAILED", error});
+    }
+});
 
 
+//delete a one user's Buffet reservation by id
+route.delete("/delete/:id", async (req, res) => {
+    const id = req.params.id;
+    try {
+        const removedSingleBuffet = await Buffet.findByIdAndDelete(id);
+        res.status(200).json({status: "SUCCESS", removedSingleBuffet});
+    } catch (error) {
+        res.status(500).json({status: "FAILED", error});
+    }
+});
 
 
 module.exports = route;
