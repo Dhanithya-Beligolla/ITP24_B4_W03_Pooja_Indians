2092157d272d418c7a694ffaf25769b3e95fc532
it22346018 <it22346018@my.sliit.lk>
2024-04-18 15:27:03 +0530

    create addrooms.js & addrooms.css components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import './App.css';
-
+import AddRooms from './componenet/addrooms';
 
 function App() {
   return (
     <div className="App">
-      <Router>
+  <Router>
+   <Header/>
+  <Routes>
+
+  <Route path="/" element={<AddRooms/>}></Route>
+    
+    
+  </Routes>  
         
-      </Router>
+  </Router>
 
    </div>
         );
