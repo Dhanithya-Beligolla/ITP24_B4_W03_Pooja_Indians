d9801c0cf966931e2d139e8a0a11f38be785050e
it22346018 <it22346018@my.sliit.lk>
2024-04-18 14:05:36 +0530

    create frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        
+      </Router>
+
+   </div>
+        );
+}
+
+export default App;
