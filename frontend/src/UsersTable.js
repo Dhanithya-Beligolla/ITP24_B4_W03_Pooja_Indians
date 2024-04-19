28449be3b17ff192151c8915d86870c1e38c9ea1
NDilusha <161825980+IT22171092RANDRanasingha@users.noreply.github.com>
2024-04-19 19:16:09 +0530

    commite 28 ( set befor tabale again to fix delet)

--- frontend/src/UsersTable.js
+++ frontend/src/UsersTable.js
@@ -1,25 +1,54 @@
-import React, { useState } from 'react';
-import { Button, Grid, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
-import SearchBar from './components/searchbar/SearchBar';
-
+import { Button, Container, Grid, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
+import { useRef } from "react";
+import {useReactToPrint} from 'react-to-print';
 
 const UsersTable = ({ rows, selectedUser, deleteUser }) => {
-    const [filteredRows, setFilteredRows] = useState(rows);
 
-    const handleSearch = (searchValue) => {
-        const filtered = rows.filter(row => row.job_title.toLowerCase().includes(searchValue.toLowerCase()));
-        setFilteredRows(filtered);
-    };
+    const ComponentsRef = useRef();
+    const handlePrint = useReactToPrint({
+        content: () => ComponentsRef.current,
+        DocumentTitele:"User Application Report",
+        onafterprint:()=>alert("User Application Report Download Successfully!"),
+    })
 
     return (
-        <TableContainer component={Paper}>
-            <Grid container justifyContent="center">
-                <Typography variant="h1" sx={{ textAlign: 'center', fontSize: '1.25rem', fontWeight: '500', }}>
-                    Application Table
+        <TableContainer 
+            component={Paper} 
+            sx={{
+                display: 'flex',
+                alignItems: 'center', 
+                justifyContent: 'center', 
+                height: '100vh',
+                }}>
+                    
+            <Table>
+                <Grid ref={ComponentsRef}>
+                    <Container
+                        sx={{
+                            border: '1px solid #CBD5E0',
+                            backgroundColor: '#f0f0f0',
+                            width: 'auto',
+                            padding: '1.25rem',
+                            display: 'flex',
+                            flexDirection: 'column', 
+                            gap: '1.25rem',
+                            borderRadius: '0.375rem',
+                            boxShadow: '0px 4px 14px rgba(0, 0, 0, 0.1)',
+                            '@media (min-width: 1024px)': {
+                              margin: 'auto', 
+                            },
+                        }}
+                    >
+                        <Grid item xs={12}>
+                            <Typography 
+                                variant="h1" sx={{
+                                textAlign: 'center', 
+                                fontSize: '1.25rem', 
+                                fontWeight: '500', 
+                            }} 
+                            >Application Table
                 </Typography>
             </Grid>
-            <SearchBar onSearch={handleSearch} />
-            <Table>
                 <TableHead>
                     <TableRow>
                         <TableCell>ID</TableCell>
@@ -35,32 +64,50 @@ const UsersTable = ({ rows, selectedUser, deleteUser }) => {
                 </TableHead>
                 <TableBody>
                     {
-                        filteredRows.length > 0 ? filteredRows.map(row => (
-                            <TableRow key={row.id} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                        rows.length > 0 ? rows.map(row => (
+                            <TableRow key={row.ID} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
                                 <TableCell component='th' scope="row">{row.id}</TableCell>
-                                <TableCell>{row.name}</TableCell>
-                                <TableCell>{row.job_title}</TableCell>
-                                <TableCell>{row.email}</TableCell>
-                                <TableCell>{row.contact_number}</TableCell>
-                                <TableCell>{row.age}</TableCell>
-                                <TableCell>{row.education_qualification}</TableCell>
-                                <TableCell>{row.work_experience}</TableCell>
+                                <TableCell component='th' scope="row">{row.name}</TableCell>
+                                <TableCell component='th' scope="row">{row.job_title}</TableCell>
+                                <TableCell component='th' scope="row">{row.email}</TableCell>
+                                <TableCell component='th' scope="row">{row.contact_number}</TableCell>
+                                <TableCell component='th' scope="row">{row.age}</TableCell>
+                                <TableCell component='th' scope="row">{row.education_qualification}</TableCell>
+                                <TableCell component='th' scope="row">{row.work_experience}</TableCell>
                                 <TableCell>
-                                    <Button sx={{ margin: '0px 10px' }} onClick={() => selectedUser(row)}>
+                                    <Button
+                                        sx={{ margin: '0px 10px' }}
+                                        onClick={() => selectedUser({ id: row.id, name: row.name, job_title: row.job_title, email: row.email, contact_number: row.contact_number, age: row.age, education_qualification: row.education_qualification, work_experience: row.work_experience })}
+                                    >
                                         UPDATE
                                     </Button>
-                                    <Button sx={{ margin: '0px 10px' }} onClick={() => deleteUser(row)}>
+
+                                    <Button
+                                        sx={{ margin: '0px 10px' }}
+                                        onClick={() => deleteUser({ id: row.id})}
+                                    >
                                         DELETE
                                     </Button>
                                 </TableCell>
                             </TableRow>
                         )) : (
-                            <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                                <TableCell colSpan={9} align="center">No data found</TableCell>
+                            <TableRow sx={{ 
+                                '&:last-child td, &:last-child th': { border: 0 } 
+                                }}>
+                                <TableCell component='th' scope="row">NO DATA</TableCell>
                             </TableRow>
                         )
                     }
                 </TableBody>
+                </Container>
+                </Grid>
+                <Grid>
+                    <Button
+                        onClick={handlePrint}
+                    >
+                        Download Report
+                    </Button>
+                </Grid>
             </Table>
         </TableContainer>
     );
