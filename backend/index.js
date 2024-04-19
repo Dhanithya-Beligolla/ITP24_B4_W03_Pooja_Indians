dbac27a0a43617bddbb10f3b074f6360c126d050
it22346018 <it22346018@my.sliit.lk>
2024-04-19 08:41:27 +0530

    merge index.js files

--- backend/index.js
+++ backend/index.js
@@ -95,8 +95,120 @@ app.get("/count_rooms", async (req, res) => {
 
 
 
+
+
+
 //////////////////////////////////////////////////////////////////////////////////////////////////////
 
+
+
+
+////////////////////////////////////////////////////////////
+
+//admin register
+const trackerregister = mongoose.Schema({
+    fname: String,
+    lname: String,
+    email: String,
+    password: String,
+
+
+
+}, {
+    timestamps: true
+
+})
+
+const registermodel = mongoose.model("admin", trackerregister)
+
+
+
+app.get("/_admin", async (req, res) => {
+    const data = await registermodel.find({})
+
+    res.json({ success: true, data: data })
+})
+
+
+app.post("/create_admin", async (req, res) => {
+    const data = new registermodel(req.body)
+    await data.save()
+    res.send({ success: true, message: "data created successfuly" })
+})
+
+
+app.put("/update_admin", async (req, res) => {
+    const { id, ...rest } = req.body
+    const data = await registermodel.updateOne({ _id: id }, rest)
+    res.send({ success: true, message: "updated successfuly", data: data })
+})
+
+
+
+
+app.delete("/delete_admin/:id", async (req, res) => {
+    const id = req.params.id
+    const data = await registermodel.deleteOne({ _id: id })
+    res.send({ success: true, message: "deleted successfully", data: data })
+})
+
+
+
+
+//update second
+app.get("/user_admin/:id", async (req, res) => {
+    const id = req.params.id;
+
+    try {
+        const discount = await registermodel.findById(id);
+
+        if (!discount) {
+            return res.status(404).send({ success: false, message: "User not found" });
+        }
+
+        res.send({ success: true, message: "User fetched successfully", data: discount });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({ success: false, message: "Internal Server Error" });
+    }
+});
+
+app.post("/login_admin", async (req, res) => {
+    console.log('in-------------------------------');
+    const { email, password } = req.body;
+
+    try {
+        console.log(email);
+        const user = await registermodel.findOne({ email });
+
+        if (!user) {
+            return res.status(404).json({ success: false, message: "User not found" });
+        }
+
+
+        // const isPasswordValid = await bcrypt.compare(password, user.password);
+        const isPasswordValid1 = user.password === password;
+
+        console.log('Input password:', password);
+        console.log('Stored hashed password:', user.password);
+        console.log('isPasswordValid:', isPasswordValid1);
+
+        if (isPasswordValid1 === false) { // Fixed condition
+            console.log('Request body:', req.body);
+            return res.status(401).json({ success: false, message: "Incorrect password" });
+
+
+        }
+
+        // If password is valid, send success message and user data
+        res.status(200).json({ success: true, message: "Login successful", data: user });
+    } catch (error) {
+        console.log('Retrieved user:', user);
+
+        console.error("Login error:", error);
+        res.status(500).json({ success: false, message: error });
+    }
+});
 
 
 
