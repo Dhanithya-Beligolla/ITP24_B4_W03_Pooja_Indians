96aedca70c063ae708bedd170176e39ee135c5e2
Dilakshi <IT22310446@my.sliit.lk>
2024-04-20 02:55:12 +0530

    Some modifications done

--- frontend/src/Pages/User_Management/WelcomeDashboard.js
+++ frontend/src/Pages/User_Management/WelcomeDashboard.js
@@ -3,7 +3,7 @@ import React from 'react'
 const WelcomeDashboard = () => {
   return (
     // <div> <h2> Welcome to Admin Dashboard </h2><br></br></div>
-    <div className='text-center rounded' style={{backgroundColor: '#00d8ff', paddingTop: '5%', paddingBottom: '4%'}}>
+    <div className='text-center rounded' style={{backgroundColor: '#fe7412', paddingTop: '5%', paddingBottom: '4%'}}>
             <h1 className='text-uppercase text-white'>Welcome To Admin Dashboard</h1>
             <span className='text-uppercase' style={{color:'#9CB693'}}></span>
         </div>
