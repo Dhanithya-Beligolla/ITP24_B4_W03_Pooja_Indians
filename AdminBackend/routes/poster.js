91edaf07592780d05c437fef19fc999bd7e79891
NDilusha <161825980+IT22171092RANDRanasingha@users.noreply.github.com>
2024-04-20 13:16:55 +0530

    commite 9 (get single poster api ok)

--- AdminBackend/routes/poster.js
+++ AdminBackend/routes/poster.js
@@ -23,4 +23,15 @@ route.post("/create", async(req, res) => {
     }
 });
 
+//get single poster
+route.get("/:id", async(req, res) => {
+    const id = req.params.id;
+    try {
+        const singlePoster = await Poster.findById(id);
+        res.status(200).json({status : "SUCCESS", singlePoster });
+    } catch (error) {
+        res.status(500).json({status : "FAILED", error});
+    }
+})
+
 module.exports = route;
