f3f7d534817b2d500d93c3533f300f679e740869
NDilusha <161825980+IT22171092RANDRanasingha@users.noreply.github.com>
2024-04-20 13:12:06 +0530

    commite 7 (create api update an commnt ok)

--- AdminBackend/routes/poster.js
+++ AdminBackend/routes/poster.js
@@ -4,10 +4,16 @@ const Poster = require("../modules/poster.js");
 const route = express.Router();
 
 //get all poster users
-route.get("/", (req, res) => {
-    res.status(200).json("this is the fist poster api.");
+route.get("/", async(req, res) => {
+    try {
+        const allPosters = await Poster.find({});
+        res.status(200).json({status : "SUCCESS", allPosters });
+    } catch (error) {
+        res.status(500).json({status : "FAILED", error});
+     }
 });
 
+//create a new poster
 route.post("/create", async(req, res) => {
     try {
         const newPoster = await Poster.create(req.body);
