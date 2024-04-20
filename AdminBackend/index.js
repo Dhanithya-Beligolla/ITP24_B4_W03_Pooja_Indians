85479bab1b172183ef8bd68f53400d642c0097e9
NDilusha <161825980+IT22171092RANDRanasingha@users.noreply.github.com>
2024-04-20 11:23:19 +0530

    commite 2 (serever cont)

---
+++ AdminBackend/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+
+const app = express();
+dotenv.config();
+
+app.listen(process.env.PORT, () => {
+    console.log("App is running at port " + process.env.PORT);
+})
