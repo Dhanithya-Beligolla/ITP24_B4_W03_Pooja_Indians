39d59b60fc0eb660de761c5b837e95f7e768a9e9
NDilusha <161825980+IT22171092RANDRanasingha@users.noreply.github.com>
2024-05-03 13:13:04 +0530

    commite 20 (defalut data remove and images upload)

--- AdminFrontend/src/components/Posters.jsx
+++ AdminFrontend/src/components/Posters.jsx
@@ -5,38 +5,7 @@ import { useQuery } from 'react-query';
 import { getAllData } from '../fetchPoster/fetchPoster';
 
 const Posters = () => {
-    const data1 = [
-        {
-            id: 1,
-            title: "Poster 1",
-            description: "Description 1",
-            image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-Vj-8TvMqBXjzqTwwhjZH0nRGt7i3JdoTl7IEl1TeuOw6wgrlRLtEyNg_--CG9Jg4pZI&usqp=CAU",
-        },
-        {
-            id: 2,
-            title: "Poster 2",
-            description: "Description 2",
-            image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-Vj-8TvMqBXjzqTwwhjZH0nRGt7i3JdoTl7IEl1TeuOw6wgrlRLtEyNg_--CG9Jg4pZI&usqp=CAU",
-        },
-        {
-            id: 3,
-            title: "Poster 3",
-            description: "Description 3",
-            image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-Vj-8TvMqBXjzqTwwhjZH0nRGt7i3JdoTl7IEl1TeuOw6wgrlRLtEyNg_--CG9Jg4pZI&usqp=CAU",
-        },
-        {
-            id: 4,
-            title: "Poster 4",
-            description: "Description 4",
-            image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-Vj-8TvMqBXjzqTwwhjZH0nRGt7i3JdoTl7IEl1TeuOw6wgrlRLtEyNg_--CG9Jg4pZI&usqp=CAU",
-        },
-        {
-            id: 5,
-            title: "Poster 5",
-            description: "Description 5",
-            image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-Vj-8TvMqBXjzqTwwhjZH0nRGt7i3JdoTl7IEl1TeuOw6wgrlRLtEyNg_--CG9Jg4pZI&usqp=CAU",
-        },
-    ];
+    
 
     const { data, isLoading, isError} = useQuery("poster", getAllData);
 
