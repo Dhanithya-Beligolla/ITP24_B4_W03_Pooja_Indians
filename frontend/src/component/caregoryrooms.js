080a3656ed1e15514ffb60062fa51cb86528eaf5
it22346018 <it22346018@my.sliit.lk>
2024-05-04 06:19:36 +0530

    update category files

--- frontend/src/component/caregoryrooms.js
+++ frontend/src/component/caregoryrooms.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './categoryrooms.css'
 
 function CategoryRooms() {
@@ -8,54 +9,49 @@ function CategoryRooms() {
             <h3>PACKAGES</h3>
             <p className="category-rooms-large">Choose a pricing package fits your needs.</p>
             <div className="category-rooms-row-padding" style={{ marginTop: '64px', display: 'flex', justifyContent: 'center' }}>
-                <div className="category-rooms-third category-rooms-section">
+            <div className="category-rooms-third category-rooms-section">
                     <ul className="category-rooms-ul category-rooms-white category-rooms-hover-shadow">
-                        <li className="category-rooms-black category-rooms-xlarge category-rooms-padding-32">Single Room</li>
-                        <li className="category-rooms-padding-16"><b>Spacious Accommodation</b></li>
-                        <li className="category-rooms-padding-16"><b>Kids' Club Access</b></li>
-                        <li className="category-rooms-padding-16"><b>Daily Meals</b></li>
-                        <li className="category-rooms-padding-16"><b>Local Excursions</b></li>
-                        {/* Removed price elements */}
-                        {/* <li className="category-rooms-padding-16">
-                            <h2 className="category-rooms-wide">$ 250</h2>
-                            <span className="category-rooms-opacity">per month</span>
-                        </li> */}
+                        <li className="category-rooms-black category-rooms-xlarge category-rooms-padding-32">SINGLE ROOM</li>
+                        <li className="category-rooms-padding-16"><b>- Accommodate One Adult</b></li>
+                        <li className="category-rooms-padding-16"><b>- Non Air Conditioner</b></li>
+                        <li className="category-rooms-padding-16"><b>- Bed Type : Single Bed</b></li>
+                        <li className="category-rooms-padding-16"><b>- Per Day : Rs.2500/=</b></li>
                         <li className="category-rooms-light-grey category-rooms-padding-24">
-                            <button className="category-rooms-button category-rooms-black category-rooms-padding-large">Sign Up</button>
+                            <Link to="/addrooms">Book Here</Link> {/* Wrap Sign Up button with Link */}
                         </li>
                     </ul>
                 </div>
                 <div className="category-rooms-third category-rooms-section">
                     <ul className="category-rooms-ul category-rooms-white category-rooms-hover-shadow">
-                        <li className="category-rooms-red category-rooms-xlarge category-rooms-padding-48">Luxury Room</li>
-                        <li className="category-rooms-padding-16"><b>Luxurious Accommodation</b></li>
-                        <li className="category-rooms-padding-16"><b>Spa & Wellness</b></li>
-                        <li className="category-rooms-padding-16"><b>Exclusive Activities</b></li>
-                        <li className="category-rooms-padding-16"><b>Airport Transfers</b></li>
+                        <li className="category-rooms-red category-rooms-xlarge category-rooms-padding-48">LUXURY ROOM</li>
+                        <li className="category-rooms-padding-16"><b>- Accommodate One / Two Adult</b></li>
+                        <li className="category-rooms-padding-16"><b>- Air Conditioner</b></li>
+                        <li className="category-rooms-padding-16"><b>- Bed Type : Thrible Bed</b></li>
+                        <li className="category-rooms-padding-16"><b>- Per Day : Rs.5000/=</b></li>
                         {/* Removed price elements */}
                         {/* <li className="category-rooms-padding-16">
                             <h2 className="category-rooms-wide">$ 500</h2>
                             <span className="category-rooms-opacity">per month</span>
                         </li> */}
                         <li className="category-rooms-light-grey category-rooms-padding-24">
-                            <button className="category-rooms-button category-rooms-black category-rooms-padding-large">Sign Up</button>
+                            <Link to="/addrooms">Book Here</Link> {/* Wrap Sign Up button with Link */}
                         </li>
                     </ul>
                 </div>
                 <div className="category-rooms-third category-rooms-section">
                     <ul className="category-rooms-ul category-rooms-white category-rooms-hover-shadow">
-                        <li className="category-rooms-black category-rooms-xlarge category-rooms-padding-32">Double Room</li>
-                        <li className="category-rooms-padding-16"><b>Comfortable Accommodation</b></li>
-                        <li className="category-rooms-padding-16"><b>Adventure Tours</b></li>
-                        <li className="category-rooms-padding-16"><b>Culinary Delights</b></li>
-                        <li className="category-rooms-padding-16"><b>Evening Entertainment</b></li>
+                        <li className="category-rooms-black category-rooms-xlarge category-rooms-padding-32">DOUBLE ROOM</li>
+                        <li className="category-rooms-padding-16"><b>- Accommodate Two Adult</b></li>
+                        <li className="category-rooms-padding-16"><b>- Non Air Conditioner</b></li>
+                        <li className="category-rooms-padding-16"><b>- Bed Type : Double Bed</b></li>
+                        <li className="category-rooms-padding-16"><b>- Per Day : Rs.3500/=</b></li>
                         {/* Removed price elements */}
                         {/* <li className="category-rooms-padding-16">
                             <h2 className="category-rooms-wide">$ 300</h2>
                             <span className="category-rooms-opacity">per month</span>
                         </li> */}
                         <li className="category-rooms-light-grey category-rooms-padding-24">
-                            <button className="category-rooms-button category-rooms-black category-rooms-padding-large">Sign Up</button>
+                            <Link to="/addrooms">Book Here</Link> {/* Wrap Sign Up button with Link */}
                         </li>
                     </ul>
                 </div>
