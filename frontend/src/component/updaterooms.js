a3d8e01d0581cd69c4f4d8be161b6321e46de1bc
it22346018 <it22346018@my.sliit.lk>
2024-05-05 04:59:31 +0530

    update update file

--- frontend/src/component/updaterooms.js
+++ frontend/src/component/updaterooms.js
@@ -1,97 +1,3 @@
-// import { useEffect, useState } from 'react'
-// import { useParams } from 'react-router-dom';
-// import './updaterooms.css'
-
-// function UpdateRooms() {
-//   const { id } = useParams();
-//   const [updaterooms, setupdateroooms] = useState({
-//     name: "",
-//     phone: "",
-//     email: "",
-//     date: "",
-//     quentity_rooms: "",
-//     quentity_people: "",
-//   })
-
-//   useEffect(() => {
-//     const fetchUserData = async () => {
-//       try {
-//         const response = await fetch(`http://localhost:8030/rooms/${id}`);
-//         const data = await response.json();
-//         console.log(data);
-
-//         if (data.success) {
-//           setupdateroooms(data.data);
-//         } else {
-//           console.error(data.message);
-//         }
-//       } catch (error) {
-//         console.error('Error fetching user data:', error);
-//       }
-//     };
-
-//     fetchUserData();
-//   }, [id]); // Include 'id' in the dependency array
-
-
-//   const handleInputChange = (e) => {
-//     setupdateroooms({
-//       ...updaterooms,
-//       [e.target.name]: e.target.value,
-//     });
-//   };
-
-//   const handleUpdate = async () => {
-//     try {
-//       const response = await fetch(`http://localhost:8030/update`, {
-//         method: 'PUT',
-//         headers: {
-//           'Content-Type': 'application/json',
-//         },
-//         body: JSON.stringify({
-//           id: updaterooms._id,
-//           ...updaterooms,
-//         }),
-//       });
-
-//       const data = await response.json();
-
-//       if (data.success) {
-//         console.log('User updated successfully');
-//         alert("updated successfully");
-
-//       } else {
-//         console.error(data.message);
-//       }
-//     } catch (error) {
-//       console.error('Error updating user:', error);
-//     }
-//   };
-
-
-//   return (
-//     <div className='rooms-update'>
-//       <h2>Update Details</h2>
-
-//       <label>Name:</label>
-//       <input type="text" id="name" name="name" onChange={handleInputChange} value={updaterooms?.name} /><br></br>
-//       <label>Phone Number:</label>
-//       <input type="number" id="phone" name="phone" onChange={handleInputChange} value={updaterooms?.phone} /><br></br>
-//       <label>Email:</label>
-//       <input type="email" id="email" name="email" onChange={handleInputChange} value={updaterooms?.email} /><br></br>
-//       <label>Date:</label>
-//       <input type="date" id="date" name="date" onChange={handleInputChange} value={updaterooms?.date} /><br></br>
-//       <label>Quantity:</label>
-//       <input type="number" min="1" max="10" id="quentity_rooms" name="quentity_rooms" onChange={handleInputChange} value={updaterooms?.quentity_rooms} /><br></br>
-//       <label>Number of Guests:</label>
-//       <input type="number" min="1" id="quentity_people" name="quentity_people" onChange={handleInputChange} value={updaterooms?.quentity_people} /><br></br>
-
-//       <button onClick={handleUpdate} >Update</button><br></br> <br></br>
-//     </div>
-//   )
-// }
-// export default UpdateRooms;
-
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
 import './updaterooms.css'
@@ -103,8 +9,8 @@ function UpdateRooms() {
     phone: "",
     email: "",
     date: "",
-    quentity_rooms: "",
-    quentity_people: "",
+    quantity_rooms: "",
+    quantity_people: "",
   })
 
   useEffect(() => {
@@ -125,8 +31,7 @@ function UpdateRooms() {
     };
 
     fetchUserData();
-  }, []);
-
+  }, [id]); // Include 'id' in the dependency array
 
 
   const handleInputChange = (e) => {
@@ -135,6 +40,7 @@ function UpdateRooms() {
       [e.target.name]: e.target.value,
     });
   };
+
   const handleUpdate = async () => {
     try {
       const response = await fetch(`http://localhost:8030/update`, {
@@ -167,26 +73,124 @@ function UpdateRooms() {
     <div className='rooms-update'>
       <h2>Update Details</h2>
 
-      <lable>Name:</lable>
+      <label>Name:</label>
       <input type="text" id="name" name="name" onChange={handleInputChange} value={updaterooms?.name} /><br></br>
-      <lable>Phone Number:</lable>
+      <label>Phone Number:</label>
       <input type="number" id="phone" name="phone" onChange={handleInputChange} value={updaterooms?.phone} /><br></br>
-      <lable>Email:</lable>
+      <label>Email:</label>
       <input type="email" id="email" name="email" onChange={handleInputChange} value={updaterooms?.email} /><br></br>
-      <lable>Date:</lable>
+      <label>Date:</label>
       <input type="date" id="date" name="date" onChange={handleInputChange} value={updaterooms?.date} /><br></br>
-      <lable>Quentity:</lable>
-      <input type="number" id="quentity_rooms" name="quentity_rooms" onChange={handleInputChange} value={updaterooms?.quentity_rooms} /><br></br>
-      <lable>Number of Guests:</lable>
-      <input type="number" id="quentity_people" name="quentity_people" onChange={handleInputChange} value={updaterooms?.quentity_people} /><br></br>
-
-
-
+      <label>Quantity:</label>
+      <input type="number" min="1" max="10" id="quantity_rooms" name="quantity_rooms" onChange={handleInputChange} value={updaterooms?.quantity_rooms} /><br></br>
+      <label>Number of Guests:</label>
+      <input type="tel" min="1" id="quantity_people" name="quantity_people" onChange={handleInputChange} value={updaterooms?.quantity_people} /><br></br>
 
       <button onClick={handleUpdate} >Update</button><br></br> <br></br>
-
-
     </div>
   )
 }
 export default UpdateRooms;
+
+
+
+
+
+// import { useEffect, useState } from 'react'
+// import { useParams } from 'react-router-dom';
+// import './updaterooms.css'
+
+// function UpdateRooms() {
+//   const { id } = useParams();
+//   const [updaterooms, setupdateroooms] = useState({
+//     name: "",
+//     phone: "",
+//     email: "",
+//     date: "",
+//     quantity_rooms: "",
+//     quantity_people: "",
+//   })
+
+//   useEffect(() => {
+//     const fetchUserData = async () => {
+//       try {
+//         const response = await fetch(`http://localhost:8030/rooms/${id}`);
+//         const data = await response.json();
+//         console.log(data);
+
+//         if (data.success) {
+//           setupdateroooms(data.data);
+//         } else {
+//           console.error(data.message);
+//         }
+//       } catch (error) {
+//         console.error('Error fetching user data:', error);
+//       }
+//     };
+
+//     fetchUserData();
+//   }, []);
+
+
+
+//   const handleInputChange = (e) => {
+//     setupdateroooms({
+//       ...updaterooms,
+//       [e.target.name]: e.target.value,
+//     });
+//   };
+//   const handleUpdate = async () => {
+//     try {
+//       const response = await fetch(`http://localhost:8030/update`, {
+//         method: 'PUT',
+//         headers: {
+//           'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify({
+//           id: updaterooms._id,
+//           ...updaterooms,
+//         }),
+//       });
+
+//       const data = await response.json();
+
+//       if (data.success) {
+//         console.log('User updated successfully');
+//         alert("updated successfully");
+
+//       } else {
+//         console.error(data.message);
+//       }
+//     } catch (error) {
+//       console.error('Error updating user:', error);
+//     }
+//   };
+
+
+//   return (
+//     <div className='rooms-update'>
+//       <h2>Update Details</h2>
+
+//       <lable>Name:</lable>
+//       <input type="text" id="name" name="name" onChange={handleInputChange} value={updaterooms?.name} /><br></br>
+//       <lable>Phone Number:</lable>
+//       <input type="number" id="phone" name="phone" onChange={handleInputChange} value={updaterooms?.phone} /><br></br>
+//       <lable>Email:</lable>
+//       <input type="email" id="email" name="email" onChange={handleInputChange} value={updaterooms?.email} /><br></br>
+//       <lable>Date:</lable>
+//       <input type="date" id="date" name="date" onChange={handleInputChange} value={updaterooms?.date} /><br></br>
+//       <lable>Quentity:</lable>
+//       <input type="number" id="quantity_rooms" name="quantity_rooms" onChange={handleInputChange} value={updaterooms?.quantity_rooms} /><br></br>
+//       <lable>Number of Guests:</lable>
+//       <input type="number" id="quantity_people" name="quantity_people" onChange={handleInputChange} value={updaterooms?.quantity_people} /><br></br>
+
+
+
+
+//       <button onClick={handleUpdate} >Update</button><br></br> <br></br>
+
+
+//     </div>
+//   )
+// }
+// export default UpdateRooms;
